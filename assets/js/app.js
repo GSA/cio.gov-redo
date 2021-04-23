1d8b66f1cfd4ec214a15197d0cb2317f6d1cae68
[arti sutaria] <[arti.sutaria@gsa.gov]>
2021-04-23 11:00:10 -0400

    Updated sidenav

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,41 +11,41 @@ $( document ).ready(function() {
     $( "ul[class^='usa-sidenav__sublist']" ).hide();
     $( "ul[id^='grandchild-sub-nav-list']" ).hide();
     
-    if(link.indexOf("cio-responsibilities") != -1){
+    if(link.indexOf("cio-responsibilities") != -1 && link.indexOf("?clickEvt") != -1){
         $('#sub-nav-list-1').show();
         $('#href-list-1').removeClass('usa-sidenav__item').addClass('usa-current');
      //   $( "ul[id^='grandchild']" ).show();
         
     }
-    if(link.indexOf("it-laws") != -1){
+    if(link.indexOf("it-laws") != -1 && link.indexOf("?clickEvt") != -1){
         $('#sub-nav-list-2').show();
         $('#href-list-2').removeClass('usa-sidenav__item').addClass('usa-current');
     }
-    if(link.indexOf("other-it-authorities") != -1){
+    if(link.indexOf("other-it-authorities") != -1 && link.indexOf("?clickEvt") != -1){
         $('#sub-nav-list-3').show();
         $('#href-list-3').removeClass('usa-sidenav__item').addClass('usa-current');
     }
-    if(link.indexOf("key-stakeholders") != -1){
+    if(link.indexOf("key-stakeholders") != -1 && link.indexOf("?clickEvt") != -1){
         $('#sub-nav-list-4').show();
         $('#href-list-4').removeClass('usa-sidenav__item').addClass('usa-current');
     }
-    if(link.indexOf("key-organizations") != -1){
+    if(link.indexOf("key-organizations") != -1 && link.indexOf("?clickEvt") != -1){
         $('#sub-nav-list-5').show();
         $('#href-list-5').removeClass('usa-sidenav__item').addClass('usa-current');
     }
-    if(link.indexOf("policies-initiatives") != -1){
+    if(link.indexOf("policies-initiatives") != -1 && link.indexOf("?clickEvt") != -1){
         $('#sub-nav-list-6').show();
         $('#href-list-6').removeClass('usa-sidenav__item').addClass('usa-current');
     }
-    if(link.indexOf("/reporting/") != -1){
+    if(link.indexOf("/reporting/") != -1 && link.indexOf("?clickEvt") != -1){
         $('#sub-nav-list-7').show();
         $('#href-list-7').removeClass('usa-sidenav__item').addClass('usa-current');
     }
-    if(link.indexOf("rep-calendar") != -1){
+    if(link.indexOf("rep-calendar") != -1 && link.indexOf("?clickEvt") != -1){
         $('#sub-nav-list-8').show();
         $('#href-list-8').removeClass('usa-sidenav__item').addClass('usa-current');
     }
-    if(link.indexOf("dditional-resources") != -1){
+    if(link.indexOf("dditional-resources") != -1 && link.indexOf("?clickEvt") != -1){
         $('#sub-nav-list-9').show();
         $('#href-list-9').removeClass('usa-sidenav__item').addClass('usa-current');
     }
