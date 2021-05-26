39e7397fd78e5e726ba8ab9cce96eda8e0624112
Luna Wang <lunawang@lunas-mbp.lan>
2021-05-26 14:47:46 -0400

    sorting dcoi cards

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,14 @@
 // Add your custom javascript here
+$( document ).ready(function() {
+
+    $('.cloud-smart .title-resources-link').sort(function(a, b) {
+        if (a.textContent < b.textContent) {
+          return -1;
+        } else {
+          return 1;
+        }
+    }).appendTo('.cloud-smart .policies');
+    
+});
+
+  
--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,15 @@
 // Add your custom javascript here
 <script src="https://unpkg.com/isotope-layout@2/dist/isotope.pkgd.min.js"></script>
+
+// Add your custom javascript here
+$( document ).ready(function() {
+    $('.cloud-smart .title-resources-link').sort(function(a, b) {
+        if (a.textContent < b.textContent) {
+          return -1;
+        } else {
+          return 1;
+        }
+    }).appendTo('body');
+});
+
+  
