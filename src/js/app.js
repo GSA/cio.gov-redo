6dd4dda0c081785c506482ed7c898960e940f299
Luna Wang <lunawang@lunas-mbp.lan>
2021-05-28 13:01:02 -0400

    pull the latest

---
+++ src/js/app.js
@@ -0,0 +1,24 @@
+// Add your custom javascript here
+
+$( document ).ready(function() {
+    
+    // sort the cards under cloud smart alphabetically
+    var cloudSmartList = $('.cloud-smart .policies-container');
+    var listitems = cloudSmartList.children('.policy-card');
+
+    listitems.sort(function(a, b) {
+    if (a.textContent < b.textContent) {
+        return -1;
+    } else {
+        return 1;
+    }
+    });
+    listitems.appendTo(cloudSmartList);
+
+
+    // add the count of the cards
+    var cardsCount = listitems.length;
+    $('.cloud-smart .filter-count').html(cardsCount + " ITEMS");
+});
+
+  
