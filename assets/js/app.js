f9247a3e8535a5ea0361cb0281fb3d2c6c72f531
Luna Wang <lunawang@lunas-mbp.lan>
2021-05-28 12:41:46 -0400

    pull the latest

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,6 @@
 // Add your custom javascript here
+<script src="https://unpkg.com/isotope-layout@2/dist/isotope.pkgd.min.js"></script>
+
 $( document ).ready(function() {
     
     // sort the cards under cloud smart alphabetically
