82baf70145ff9a0ca5902e2c0173b35c79692edd
root <root@LAPTOP-SEJBC8JT.localdomain>
2021-06-15 12:19:45 -0700

    Changes to questions aia-eia-js

---
+++ aia-eia-js/precache-manifest.69aa38d1c378ac8f6fc8586bf2759add.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a571c1ffd4a69804ea76",
+    "url": "/aia-eia-js/css/app.ecb6e467.css"
+  },
+  {
+    "revision": "98a046ebb601383c2271",
+    "url": "/aia-eia-js/css/chunk-vendors.bdd50369.css"
+  },
+  {
+    "revision": "036a9dfdbd62be8cc54d35a31232bf61",
+    "url": "/aia-eia-js/helper/browser.js"
+  },
+  {
+    "revision": "34dca1724d71a51ed75c91820a012df7",
+    "url": "/aia-eia-js/helper/pdfmake.min.js"
+  },
+  {
+    "revision": "f26b7c03c514d3701f255a4dfe6c0dd1",
+    "url": "/aia-eia-js/helper/startswith.js"
+  },
+  {
+    "revision": "f88fe8576fb527a53c81799d20428ef0",
+    "url": "/aia-eia-js/helper/vfs_fonts.js"
+  },
+  {
+    "revision": "ba0ddff885d13950f3ed8872584759bd",
+    "url": "/aia-eia-js/helper/wet.js"
+  },
+  {
+    "revision": "d0652b4db22232ae88d5615b6c8ad13d",
+    "url": "/aia-eia-js/img/angle-arrow-down-primary.svg"
+  },
+  {
+    "revision": "d1c68805b39204bd12c74fb89cf46b5a",
+    "url": "/aia-eia-js/img/icon-dot-gov.svg"
+  },
+  {
+    "revision": "e012deca8c4f6e0fc9e2e269f06e16c1",
+    "url": "/aia-eia-js/img/icon-https.svg"
+  },
+  {
+    "revision": "be327dc2791d8096d69c7737a1d0a2b8",
+    "url": "/aia-eia-js/img/us_flag_small.png"
+  },
+  {
+    "revision": "a36e9187ab70aea48f8b868c788069ea",
+    "url": "/aia-eia-js/index.html"
+  },
+  {
+    "revision": "a571c1ffd4a69804ea76",
+    "url": "/aia-eia-js/js/app.fc5e0a66.js"
+  },
+  {
+    "revision": "98a046ebb601383c2271",
+    "url": "/aia-eia-js/js/chunk-vendors.4b1c4b62.js"
+  },
+  {
+    "revision": "e3d984ba4da736a0c875e10e88363984",
+    "url": "/aia-eia-js/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/aia-eia-js/robots.txt"
+  },
+  {
+    "revision": "5e4cb68db7d62e2146e8a8bd2fe623be",
+    "url": "/aia-eia-js/survey.css"
+  }
+]);
