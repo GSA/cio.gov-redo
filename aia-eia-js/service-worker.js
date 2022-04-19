99c7ebe7d537e53155c44ec814bb097046387a8f
Trigve Hagen <trigve.hagen@gsa.gov>
2022-04-19 09:59:15 -0700

    Added mailto link.

--- aia-eia-js/service-worker.js
+++ aia-eia-js/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/aia-eia-js/precache-manifest.120a6d523669469441d4202f5538f7fd.js"
+  "/aia-eia-js/precache-manifest.1f61250c7f99b461f9760c0f35c2b5e0.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "aia-eia-js"});
